6bad4db771cad63017cfdc11815ed6675acb3d84
6bstudent <fki2066206@stu.o-hara.ac.jp>
2021-09-24 15:22:38 +0900

    ログイン画面ベース作成

--- app/src/main/java/com/example/zazen/activity/StartActivity.java
+++ app/src/main/java/com/example/zazen/activity/StartActivity.java
@@ -4,24 +4,30 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.zazen.R;
+import com.example.zazen.async.HttpRequest_GET;
 
 public class StartActivity extends AppCompatActivity {
     private Button loginButton;
     private TextView username,userid,accountIcon;
-
+    static SharedPreferences loginStatus;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        loginButton = findViewById(R.id.loginMenuButton);
+        username=findViewById(R.id.username);
+        userid=findViewById(R.id.userid);
+        HttpRequest_GET httpRequestPost = new HttpRequest_GET(this);
+        httpRequestPost.execute("http://fukuiohr2.sakura.ne.jp/2021/Zazen/postdata.php");
 
     }
 
