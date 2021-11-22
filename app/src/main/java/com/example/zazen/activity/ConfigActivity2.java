95f2806a091c7c7e4dc5cf0ea521327c166a0743
6bstudent <fki2066206@stu.o-hara.ac.jp>
2021-11-22 16:44:36 +0900

    フリーモード設定レイアウトまで

---
+++ app/src/main/java/com/example/zazen/activity/ConfigActivity2.java
@@ -0,0 +1,15 @@
+package com.example.zazen.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.zazen.R;
+
+public class ConfigActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config2);
+    }
+}
