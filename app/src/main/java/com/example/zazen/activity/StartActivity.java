e5d199174904898d935f1803aa2170392439ce27
6bstudent <fki2066206@stu.o-hara.ac.jp>
2021-11-22 16:21:41 +0900

    モード選択の動作まで

--- app/src/main/java/com/example/zazen/activity/StartActivity.java
+++ app/src/main/java/com/example/zazen/activity/StartActivity.java
@@ -14,23 +14,28 @@ import android.widget.TextView;
 import com.example.zazen.R;
 
 public class StartActivity extends AppCompatActivity {
-    private Button startButton, ruleButton, loginMenuButton;
-    private ImageButton accountIcon;
+    private Button startButton, webButton, loginMenuButton, nomalModeButton, freeModeButton;
+    private ImageButton accountIcon, help;
     private TextView username, userid;
-    private View loginMenu, loginScreen;
+    private View loginMenu, loginScreen, selectionScreen;
 
     public static SharedPreferences loginStatus;
     public static SharedPreferences.Editor login_editor;
 
-    private boolean accountOpenFlg = true, loginOpenFlg = false;
+    private boolean accountOpenFlg = true, loginOpenFlg = false, selectionOpenFlg = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ruleButton = findViewById(R.id.webButton);
+        webButton = findViewById(R.id.webButton);
         loginScreen = findViewById(R.id.loginScreen);
+
+        selectionScreen = findViewById(R.id.selectionScreen);
+        nomalModeButton = findViewById(R.id.nomalModeButton);
+        freeModeButton = findViewById(R.id.freeModeButton);
+        help = findViewById(R.id.help);
 
         loginMenu = findViewById(R.id.loginMenu);
         loginMenuButton = findViewById(R.id.loginMenuButton);
@@ -66,10 +71,16 @@ public class StartActivity extends AppCompatActivity {
                         .show();
             } else {
                 loginOpenFlg = true;
-                startButton.setEnabled(false);
-                ruleButton.setEnabled(false);
                 loginScreen.setVisibility(View.VISIBLE);
             }
+        });
+
+        startButton.setOnClickListener(v -> {
+            selectionOpenFlg = true;
+            selectionScreen.setVisibility(View.VISIBLE);
+        });
+
+        selectionScreen.setOnClickListener(v -> {
         });
     }
 
@@ -109,27 +120,57 @@ public class StartActivity extends AppCompatActivity {
     }
 
     public void config(View v) {
-        Intent intent = new Intent(getApplicationContext(), ConfigActivity.class);
+        Intent intent = null;
+        switch (getResources().getResourceEntryName(v.getId())) {
+            case "nomalModeButton":
+                intent = new Intent(getApplicationContext(), ConfigActivity.class);
+                startActivity(intent);
+                break;
+            case "freeModeButton":
+                intent = new Intent(getApplicationContext(), ConfigActivity.class);
+                startActivity(intent);
+                break;
+        }
+        selectionOpenFlg = false;
+        selectionScreen.setVisibility(View.GONE);
+    }
+
+    public void web(View v) {
+        Intent intent = new Intent(getApplicationContext(), WebActivity.class);
         startActivity(intent);
     }
 
-    public void web(View v){
-        Intent intent = new Intent(getApplicationContext(), WebActivity.class);
-        startActivity(intent);
+    public void help(View v) {
+        switch (getResources().getResourceEntryName(v.getId())) {
+            case "help":
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setTitle("モード")
+                        .setMessage("モードの選択をします。\n" +
+                                "記録あり：\n　・座禅の記録を行います。\n" +
+                                "　・記録は任意でデータベースに送信でき、Web版にて記録を見ることができます。\n" +
+                                "　・ジャイロ計測による呼吸の練習もできます。\n" +
+                                "記録なし：\n　・記録は残りません。\n" +
+                                "　・マインドフルネス瞑想など、瞑想等のみに集中したい方向けです。\n" +
+                                "　・時間も体勢も自分の好みに合わせて行うことができます。")
+                        .setPositiveButton("閉じる", null)
+                        .show();
+                break;
+        }
     }
 
     public void onBackPressed() {
         if (loginOpenFlg) {
             loginOpenFlg = false;
-            startButton.setEnabled(true);
-            ruleButton.setEnabled(true);
             loginScreen.setVisibility(View.GONE);
             if (loginStatus.getBoolean("LoginFlg", false)) {
                 login();
             }
+        } else if (selectionOpenFlg) {
+            selectionOpenFlg = false;
+            selectionScreen.setVisibility(View.GONE);
         } else {
             new AlertDialog.Builder(this)
-                    //.setTitle("開始前確認")
                     .setCancelable(false)
                     .setMessage("アプリを終了しますか？")
                     .setPositiveButton("はい", (dialog, which) -> {
