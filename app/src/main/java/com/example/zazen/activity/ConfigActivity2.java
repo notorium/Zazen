e609a890e4d2ee9e77e14d0118d393b8e0f289c8
6bstudent <fki2066206@stu.o-hara.ac.jp>
2021-11-25 16:26:57 +0900

    記録なしモードメイン動作

--- app/src/main/java/com/example/zazen/activity/ConfigActivity2.java
+++ app/src/main/java/com/example/zazen/activity/ConfigActivity2.java
@@ -51,7 +51,7 @@ public class ConfigActivity2 extends AppCompatActivity {
 
         setTime.setProgress(config_value2.getInt("SeekValue", 0));
         timeEditText.setText(Integer.toString(config_value2.getInt("TextValue", 10)));
-        setShowTimer.setChecked(config_value2.getBoolean("TimeChecked", false));
+        setShowTimer.setChecked(config_value2.getBoolean("TimeChecked", true));
         setTimeSelect.check(config_value2.getInt("Select", R.id.radioButton1));
 
         switch (setTimeSelect.getCheckedRadioButtonId()) {
@@ -70,27 +70,29 @@ public class ConfigActivity2 extends AppCompatActivity {
         }
 
         //EditTextのフォーカスを外す
-        config2.setOnClickListener(v -> {
-            timeEditText.clearFocus();
-            editor2.putInt("TextValue", Integer.parseInt(String.valueOf(timeEditText.getText()))).apply();
-        });
+//        config2.setOnClickListener(v -> {
+//            timeEditText.clearFocus();
+//            textCheck();
+//        });
 
         //時間の詳細設定(ラジオボタン)
         setTimeSelect.setOnCheckedChangeListener(
                 (group, checkedId) -> {
-                    editor2.putInt("TextValue", Integer.parseInt(String.valueOf(timeEditText.getText()))).apply();
+                    textCheck();
                     switch (checkedId) {
                         case R.id.radioButton1:
                             setTime.setEnabled(true);
                             upTime.setEnabled(false);
                             downTime.setEnabled(false);
                             timeEditText.setEnabled(false);
+                            editor2.putInt("SelectNum", 1).apply();
                             break;
                         case R.id.radioButton2:
                             setTime.setEnabled(false);
                             upTime.setEnabled(true);
                             downTime.setEnabled(true);
                             timeEditText.setEnabled(true);
+                            editor2.putInt("SelectNum", 2).apply();
                             break;
                     }
                     //ラジオボタン選択時の処理
@@ -110,7 +112,7 @@ public class ConfigActivity2 extends AppCompatActivity {
                     @Override
                     public void onStartTrackingTouch(SeekBar seekBar) {
                         //ツマミに触ったときの処理
-                        editor2.putInt("TextValue", Integer.parseInt(String.valueOf(timeEditText.getText()))).apply();
+                        textCheck();
                         System.out.println(setTime.getProgress());
                     }
 
@@ -129,6 +131,9 @@ public class ConfigActivity2 extends AppCompatActivity {
                 tmp++;
                 timeEditText.setText(Integer.toString(tmp));
                 editor2.putInt("TextValue", tmp).apply();
+            } else {
+                timeEditText.setText("1");
+                editor2.putInt("TextValue", 1).apply();
             }
         });
 
@@ -138,21 +143,38 @@ public class ConfigActivity2 extends AppCompatActivity {
                 tmp--;
                 timeEditText.setText(Integer.toString(tmp));
                 editor2.putInt("TextValue", tmp).apply();
+            } else {
+                timeEditText.setText("999");
+                editor2.putInt("TextValue", 999).apply();
             }
         });
 
         //時間表示設定(スイッチ)
         setShowTimer.setOnCheckedChangeListener((v, isChecked) -> {
-            editor2.putInt("TextValue", Integer.parseInt(String.valueOf(timeEditText.getText()))).apply();
+            textCheck();
             if (setShowTimer.isChecked()) {
                 //スイッチON
-                editor2.putBoolean("GyroChecked", true).apply();
+                editor2.putBoolean("TimeChecked", true).apply();
             } else {
                 //スイッチOFF
-                editor2.putBoolean("GyroChecked", false).apply();
+                editor2.putBoolean("TimeChecked", false).apply();
             }
             editor2.commit();
         });
+    }
+
+    public void textCheck() {
+        int num;
+        try {
+            num = Integer.valueOf(timeEditText.getText().toString());
+            if (num <= 0) {
+                num = 1;
+            }
+        } catch (NumberFormatException e) {
+            num = 1;
+        }
+        timeEditText.setText(Integer.toString(num));
+        editor2.putInt("TextValue", num).apply();
     }
 
     public void help(View v) {
@@ -160,49 +182,18 @@ public class ConfigActivity2 extends AppCompatActivity {
             case "help1":
                 new AlertDialog.Builder(this)
                         .setCancelable(false)
-                        .setTitle("モード")
-                        .setMessage("モードの選択をします。\n" +
-                                "練習モード：\n　呼吸の練習をするモードです。\n" +
-                                "　時間：3分　ジャイロ：あり\n" +
-                                "　設定値は固定です。\n" +
-                                "瞑想モード：\n　ジャイロ計測をしないモードです。\n" +
-                                "　自由な体勢で瞑想ができます。\n" +
-                                "　ジャイロ計測はOFF固定です。\n" +
-                                "　時間と端末の設置場所を自由に設定できます。\n" +
-                                "フリーモード：\n　設定値をカスタムできるモードです。\n" +
-                                "　設定値を自由に設定できます。\n")
+                        .setTitle("時間設定")
+                        .setMessage("・規定値、指定なしまたはカスタムで時間を設定できます。\n" +
+                                "・時間指定なしの上限は999分です。\n" +
+                                "・カスタムで設定できる時間は1～999分です。")
                         .setPositiveButton("閉じる", null)
                         .show();
                 break;
             case "help2":
                 new AlertDialog.Builder(this)
                         .setCancelable(false)
-                        .setTitle("時間")
-                        .setMessage("座禅を行う時間を設定します。\n" +
-                                "時間指定ありの場合：\n　・設定時間分の記録を行います。\n" +
-                                "　・中断すると記録が残りません。\n" +
-                                "時間指定なしの場合：\n　・記録できる時間の上限は60分です。\n" +
-                                "　・開始から1分未満で中断すると記録が残りません。\n" +
-                                "　・開始から1分以上から中断しても記録が残るようになります。")
-                        .setPositiveButton("閉じる", null)
-                        .show();
-                break;
-            case "help3":
-                new AlertDialog.Builder(this)
-                        .setCancelable(false)
-                        .setTitle("ジャイロ")
-                        .setMessage("ジャイロの計測を行うかを設定します。" +
-                                "\n・計測をONにする場合は、端末の設置場所は手の上固定となります。" +
-                                "\n・瞑想モードの場合はジャイロなし固定です。")
-                        .setPositiveButton("閉じる", null)
-                        .show();
-                break;
-            case "help4":
-                new AlertDialog.Builder(this)
-                        .setCancelable(false)
-                        .setTitle("端末の設置場所")
-                        .setMessage("座禅をする際の端末の設置場所を設定します。\n" +
-                                "・ジャイロ計測ONの場合は手の上固定となります。")
+                        .setTitle("時間表示設定")
+                        .setMessage("・画面に時間を表示するか設定します。")
                         .setPositiveButton("閉じる", null)
                         .show();
                 break;
@@ -210,14 +201,14 @@ public class ConfigActivity2 extends AppCompatActivity {
     }
 
     public void start(View v) {
-        editor2.putInt("TextValue", Integer.parseInt(String.valueOf(timeEditText.getText()))).apply();
+        textCheck();
         //開始前確認メッセージ
         new AlertDialog.Builder(this)
                 .setTitle("開始前確認")
                 .setCancelable(false)
                 .setMessage("この設定で開始しますか？")
                 .setPositiveButton("はい", (dialog, which) -> {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
                     startActivity(intent);
                     this.finish();
                 })
--- app/src/main/java/com/example/zazen/activity/MainActivity2.java
+++ app/src/main/java/com/example/zazen/activity/MainActivity2.java
@@ -1,16 +1,371 @@
 package com.example.zazen.activity;
 
+import android.content.Intent;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 import com.example.zazen.R;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity2 extends AppCompatActivity {
 
+    //View変数
+    private TextView timerText, insteadtimerText, countdownText, attentionText;
+    private Button resultButton;
+    private View startScreen, poseScreen, tapScreen, mode1Dialog;
+
+    //座禅開始判定
+    private boolean activityStart = false;
+
+    //座禅終了判定
+    private boolean activityFinish = false;
+
+    //ポーズ判定
+    private boolean activityPose = true;
+
+    //音声データ
+    private SoundPool soundPool;
+    private int se1, se2, se3;
+
+    //データフォーマット
+//    private SimpleDateFormat dataFormat =
+//            new SimpleDateFormat("mm:ss.SS", Locale.JAPAN);
+
+    //タイマー変数
+    private CountDown countDown;
+    private CountUp countUp;
+    private long[] countNumberList = {180000, 300000, 600000, 1200000, 1800000, 59940000};
+    private long countNumber = 0;
+    private long firstTime = 0;
+    private long oldSec = 0;
+    private Boolean countUpDownFlag;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        View decor = getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        timerText = findViewById(R.id.timerText);
+        countdownText = findViewById(R.id.countdownText);
+        attentionText = findViewById(R.id.attentionText);
+        insteadtimerText = findViewById(R.id.insteadtimerText);
+        resultButton = findViewById(R.id.resultButton);
+
+        startScreen = findViewById(R.id.startScreen);
+        poseScreen = findViewById(R.id.poseScreen);
+        tapScreen = findViewById(R.id.tapScreen);
+
+        attentionText.setText("記録は行われません。\n楽な体勢で行ってください。");
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        } else {
+            AudioAttributes attr = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build();
+            soundPool = new SoundPool.Builder()
+                    .setAudioAttributes(attr)
+                    .setMaxStreams(5)
+                    .build();
+        }
+
+        se1 = soundPool.load(this, R.raw.se1, 1);
+        se2 = soundPool.load(this, R.raw.se2, 1);
+        se3 = soundPool.load(this, R.raw.se3, 1);
+
+        if (!ConfigActivity2.config_value2.getBoolean("TimeChecked", true)) {
+            timerText.setVisibility(View.GONE);
+            insteadtimerText.setVisibility(View.VISIBLE);
+        }
+
+        if (ConfigActivity2.config_value2.getInt("SelectNum", 1) == 1) {
+            countUpDownFlag = ConfigActivity2.config_value2.getInt("SeekValue", 0) != 5;
+            countNumber = countNumberList[ConfigActivity2.config_value2.getInt("SeekValue", 0)];
+        } else {
+            countUpDownFlag = true;
+            countNumber = ConfigActivity2.config_value2.getInt("TextValue", 10) * 60000;
+        }
+        firstTime = countNumber;
+
+        if (countUpDownFlag) {
+            long milli = countNumber % 1000 / 10;
+            long second = (countNumber / 1000) % 60;
+            long minute = (countNumber / (1000 * 60));
+            timerText.setText(String.format("%02d:%02d.%02d", minute, second, milli));
+        }
+
+
+        countdownText.setText("");
+
+        if (countUpDownFlag) {
+            countDown = new CountDown(countNumber, 10);
+        } else {
+            countUp = new CountUp(countNumber, 10, firstTime);
+        }
+    }
+
+    //画面タップ後に座禅スタート
+    public void screenTap(View v) {
+        if (!activityStart && !activityFinish) {
+            //3秒のカウントダウン
+            countDown();
+        } else if (activityStart && !activityFinish) {
+            poseScreen.setVisibility(View.VISIBLE);
+            tapScreen.setEnabled(false);
+
+//            if (countUpDownFlag) {
+//                countDown.cancel();
+//            } else {
+//                countTimer.cancel();
+//            }
+
+//            activityStart = false;
+            activityPose = true;
+        }
+    }
+
+
+    //タップから3秒後にスタート
+    public void countDown() {
+        startScreen.setVisibility(View.GONE);
+        tapScreen.setEnabled(false);
+        activityPose = false;
+
+        final Handler countDownHandler = new Handler();
+        countDownHandler.postDelayed(() -> countdownText.setText("3"), 1000);
+        countDownHandler.postDelayed(() -> countdownText.setText("2"), 2000);
+        countDownHandler.postDelayed(() -> countdownText.setText("1"), 3000);
+        countDownHandler.postDelayed(() -> {
+            timerText.setEnabled(true);
+            insteadtimerText.setEnabled(true);
+            countdownText.setText("Start!!");
+        }, 4000);
+
+        countDownHandler.postDelayed(() -> {
+            countdownText.setText("");
+            if (!activityFinish) {
+                if (countUpDownFlag) {
+                    countDown.start();
+                } else {
+                    countUp.start();
+                }
+                tapScreen.setEnabled(true);
+            }
+            activityStart = true;
+        }, 5000);
+    }
+
+    //ポーズ画面
+    public void pose(View v) {
+        switch (getResources().getResourceEntryName(v.getId())) {
+            case "resume":
+                //再開
+                poseScreen.setVisibility(View.GONE);
+                tapScreen.setEnabled(true);
+                break;
+
+            case "restart":
+                //座禅やり直し
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setMessage("同じ設定のままやり直しますか？")
+                        .setPositiveButton("はい", (dialog, which) -> {
+                            onStop();
+                            if (countUpDownFlag) {
+                                countDown.cancel();
+                            } else {
+                                countUp.cancel();
+                            }
+                            Intent intent = getIntent();
+                            overridePendingTransition(0, 0);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                            finish();
+
+                            overridePendingTransition(0, 0);
+                            startActivity(intent);
+                        })
+                        .setNegativeButton("いいえ", null)
+                        .show();
+                break;
+
+            case "finish":
+                //座禅中断
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setMessage("座禅を終了しますか？")
+                        .setPositiveButton("はい", (dialog, which) -> {
+                            Intent intent = new Intent(getApplicationContext(), ResultActivity2.class);
+                            SimpleDateFormat DF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.JAPAN);
+                            long second = (countNumber / 1000) % 60;
+                            long minute = (countNumber / (1000 * 60)) % 60;
+                            long hour = (countNumber / (1000 * 60 * 60)) % 24;
+                            intent.putExtra("SetTime", String.format("%02d:%02d:%02d.%d", hour, minute, second, countNumber % 1000));
+//                                intent.putExtra("StartTime", DF.format(startTime));
+                            intent.putExtra("Minute", (second == 0 ? "" : "約") + (hour * 60 + minute));
+                            startActivity(intent);
+                            if (countUpDownFlag) {
+                                countDown.cancel();
+                            } else {
+                                countUp.cancel();
+                            }
+                            onStop();
+                            activityFinish = true;
+                            this.finish();
+                        })
+                        .setNegativeButton("いいえ", null)
+                        .show();
+        }
+    }
+
+    //結果画面へ遷移
+    public void result(View v) {
+        Intent intent = new Intent(getApplicationContext(), ResultActivity2.class);
+        SimpleDateFormat DF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.JAPAN);
+        long second = (firstTime / 1000) % 60;
+        long minute = (firstTime / (1000 * 60)) % 60;
+        long hour = (firstTime / (1000 * 60 * 60)) % 24;
+        intent.putExtra("SetTime", String.format("%02d:%02d:%02d.%d", hour, minute, second, firstTime % 1000));
+//        intent.putExtra("StartTime", DF.format(startTime));
+        intent.putExtra("Minute", Long.toString(hour * 60 + minute));
+        soundPool.stop(se3);
+        startActivity(intent);
+        this.finish();
+    }
+
+    //ホームボタン、タスクボタンタップ検知
+    public void onUserLeaveHint() {
+        //座禅強制終了
+        if (!activityFinish) {
+            if (countUpDownFlag) {
+                countDown.cancel();
+            } else {
+                countUp.cancel();
+            }
+            activityStart = false;
+            activityFinish = true;
+            onStop();
+            new AlertDialog.Builder(this)
+                    .setCancelable(false)
+                    .setMessage("座禅が中断されました")
+                    .setPositiveButton("閉じる", (dialog, which) -> {
+                        this.finish();
+                    })
+                    .show();
+        }
+    }
+
+    //戻るボタン処理
+    public void onBackPressed() {
+        if (activityStart && !activityFinish) {
+            //画面タップ有効時、ポーズ画面へ遷移
+            poseScreen.setVisibility(View.VISIBLE);
+            tapScreen.setEnabled(false);
+        }
+    }
+
+    //カウントダウンタイマー
+    class CountDown extends CountDownTimer {
+
+        CountDown(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onFinish() {
+            //タイマー終了
+            activityStart = false;
+            activityFinish = true;
+            soundPool.play(se3, 1f, 1f, 0, 3, 1f);
+            startScreen.setVisibility(View.GONE);
+            tapScreen.setVisibility(View.GONE);
+            poseScreen.setVisibility(View.GONE);
+            timerText.setText(String.format("%02d:%02d.%02d", 0, 0, 0));
+            countdownText.setText("終了!!");
+            resultButton.setVisibility(View.VISIBLE);
+        }
+
+        // インターバルで呼ばれる
+        @Override
+        public void onTick(long millisUntilFinished) {
+            // 残り時間を分、秒、ミリ秒に分割
+            countNumber = millisUntilFinished;
+            long milli = countNumber % 1000 / 10;
+            long second = (countNumber / 1000) % 60;
+            long minute = (countNumber / (1000 * 60));
+            timerText.setText(String.format("%02d:%02d.%02d", minute, second, milli));
+        }
+    }
+
+    //カウントアップタイマー
+    class CountUp extends CountDownTimer {
+        private final long firstTime;
+
+        public CountUp(long millisInFuture, long countDownInterval, long firstTime) {
+            super(millisInFuture, countDownInterval);
+            this.firstTime = firstTime;
+        }
+
+        @Override
+        public void onFinish() {
+            //タイマー終了
+            activityStart = false;
+            activityFinish = true;
+            soundPool.play(se3, 1f, 1f, 0, 3, 1f);
+            startScreen.setVisibility(View.GONE);
+            tapScreen.setVisibility(View.GONE);
+            poseScreen.setVisibility(View.GONE);
+            long milli = firstTime % 1000 / 10;
+            long second = (firstTime / 1000) % 60;
+            long minute = (firstTime / (1000 * 60));
+            timerText.setText(String.format("%02d:%02d.%02d", minute, second, milli));
+            countdownText.setText("終了!!");
+            resultButton.setVisibility(View.VISIBLE);
+        }
+
+        // インターバルで呼ばれる
+        @Override
+        public void onTick(long millisUntilFinished) {
+            countNumber = firstTime - millisUntilFinished;
+            if (countNumber >= firstTime) {
+                onFinish();
+            }
+            long milli = countNumber % 1000 / 10;
+            long second = (countNumber / 1000) % 60;
+            long minute = (countNumber / (1000 * 60));
+//            if (second != oldSec) {
+//                oldSec = second;
+//                if (oldSec % 10 == 0) {
+//                    soundPool.play(se2, 1f, 1f, 0, 0, 1f);
+//                }
+//                soundPool.play(se1, 1f, 1f, 0, 0, 1f);
+//            }
+            timerText.setText(String.format("%02d:%02d.%02d", minute, second, milli));
+        }
     }
 }
+
+
